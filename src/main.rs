054fff84227dc97e64a1f331019f88dcd75b7e8e
aliabdelrehim <aliabdelrehim7890@gmail.com>
2025-09-25 11:38:29 +0300

    add standard library to access environment variables + mp3 songs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Successfully created CLI Audio project repo.");
+/* 
+import env module from the standard library
+env: interact with the environment
+*/ 
+use std::env::current_dir;
+
+fn main() -> std::io::Result<()> {
+    let path = current_dir()?;
+    println!("The current directory is {}", path.display());
+    Ok(()) 
 }
