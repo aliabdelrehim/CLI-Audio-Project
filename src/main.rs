5d55708df595d2da37009070d01bf016f6e7f1b0
aliabdelrehim <aliabdelrehim7890@gmail.com>
2025-09-25 18:18:54 +0300

    comments for code illustration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 /* 
 import env module from the standard library
 env: interact with the environment
+calling current_dir function to get the current working directory
+from the standard library environment module
 */ 
 use std::env::current_dir;
+
 
 fn main() -> std::io::Result<()> {
     let path = current_dir()?;
