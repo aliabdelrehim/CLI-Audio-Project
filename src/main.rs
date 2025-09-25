3e09adab1a4f09526cae8492369d0d6e9f1b02ef
aliabdelrehim <aliabdelrehim7890@gmail.com>
2025-09-25 18:20:22 +0300

    comments for code illustration

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,15 @@ from the standard library environment module
 */ 
 use std::env::current_dir;
 
-
+/*main function returns a result type that is either Ok
+or an error type std::io::Error
+std::io: standard input/output library
+*/
 fn main() -> std::io::Result<()> {
+
+    //? operator handles the results (Ok or Error)
     let path = current_dir()?;
     println!("The current directory is {}", path.display());
+    //code exectution is successful if it reaches ok function (success exit)
     Ok(()) 
 }
