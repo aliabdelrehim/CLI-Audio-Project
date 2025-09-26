fbdc8ea544f7c848edf7ecc911a5df939cf607a8
aliabdelrehim <aliabdelrehim7890@gmail.com>
2025-09-26 08:09:45 +0300

    Test playing 3 songs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ from the standard library environment module
 */ 
 use std::env::current_dir;
 use std::fs::File;
-use rodio::{Decoder, OutputStream, source::Source};
+use rodio::{Decoder};
 
 /*main function returns a result type that is either Ok
 or an error type std::io::Error
@@ -22,19 +22,26 @@ fn main() -> std::io::Result<()> {
     // Note that the playback stops when the stream_handle is dropped.//!
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()
             .expect("open default audio stream");
-    let sink = rodio::Sink::connect_new(&stream_handle.mixer());
+    let _sink = rodio::Sink::connect_new(&stream_handle.mixer());
     // Load a sound from a file, using a path relative to Cargo.toml
-    let file = File::open("Blue_One_Love.mp3").unwrap();
+    let file1 = File::open("Blue_One_Love.mp3").unwrap();
+    let file2 = File::open("coldplay_a-sky-full-of-stars-coldplay.mp3").unwrap();
+    let file3 = File::open("Show_Me_The_Meaning_Of_Being_Lonely.mp3").unwrap();
     // Decode that sound file into a source
-    let source = Decoder::try_from(file).unwrap();
+    let source1 = Decoder::try_from(file1).unwrap();
+    let source2 = Decoder::try_from(file2).unwrap();
+    let source3 = Decoder::try_from(file3).unwrap();
     // Play the sound directly on the device
-    stream_handle.mixer().add(source);
+    stream_handle.mixer().add(source1);
+    stream_handle.mixer().add(source2);
+    stream_handle.mixer().add(source3);
 
     // The sound plays in a separate audio thread,
     // so we need to keep the main thread alive while it's playing.
     println!("Playing sound for 5 seconds...");
-    std::thread::sleep(std::time::Duration::from_secs(20));
-    println!("Playing sound for 5 seconds...");
+    std::thread::sleep(std::time::Duration::from_secs(5));
+    
+    
     //code exectution is successful if it reaches ok function (success exit)
     Ok(()) 
 }
